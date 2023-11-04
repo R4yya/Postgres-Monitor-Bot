97cd825c31577826a5c06d603125b04aa0ae7735
R4yya <121268707+R4yya@users.noreply.github.com>
2023-11-04 03:53:06 +0300

    code refactoring

--- bot_test.py
+++ bot_test.py
@@ -6,12 +6,13 @@ import psycopg2
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler, CallbackContext
 
+# Logger
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-
+# Global variables
 selected_database = None
 selected_metric = None
 
@@ -25,7 +26,7 @@ def create_db_connection():
             password=os.getenv('PASSWORD')
         )
     except Exception as e:
-        logging.error("An error occurred: %s", str(e))
+        logging.error(f'An error occurred: {str(e)}')
 
 
 def execute_sql_query(connection, query):
@@ -37,16 +38,17 @@ def execute_sql_query(connection, query):
 def get_database_list():
     try:
         connection = create_db_connection()
-        query = "SELECT datname FROM pg_database;"
+        query = 'SELECT datname FROM pg_database;'
         database_list = execute_sql_query(connection, query)
 
         if database_list:
             return [row[0] for row in database_list]
         else:
             return []
-
     except Exception as e:
-        logging.error("An error occurred: %s", str(e))
+        logging.error(f'An error occurred: {str(e)}')
+
+        return f'An error occurred while retrieving database list.'
 
 
 def get_active_sessions(database_name):
@@ -59,9 +61,10 @@ def get_active_sessions(database_name):
             return result[0][0]
         else:
             return 0
-
     except Exception as e:
-        logging.error("An error occurred: %s", str(e))
+        logging.error(f'An error occurred: {str(e)}')
+
+        return f'An error occurred while retrieving session information.'
 
 
 def get_sessions_with_lwlock(database_name):
@@ -74,9 +77,10 @@ def get_sessions_with_lwlock(database_name):
             return result[0][0]
         else:
             return 0
-
     except Exception as e:
-        logging.error("An error occurred: %s", str(e))
+        logging.error(f'An error occurred: {str(e)}')
+
+        return f'An error occurred while retrieving session information.'
 
 
 def get_longest_transaction_duration(database_name):
@@ -88,18 +92,18 @@ def get_longest_transaction_duration(database_name):
         if result:
             return result[0][0]
         else:
-            return "No active transactions found."
-
+            return 'No active transactions found.'
     except Exception as e:
-        logging.error("An error occurred: %s", str(e))
-        return f"An error occurred while retrieving transaction information. {e}"
+        logging.error(f'An error occurred: {str(e)}')
+
+        return f'An error occurred while retrieving transaction information.'
 
 
 def create_database_buttons(database_list):
     buttons = []
 
     for database in database_list:
-        button = InlineKeyboardButton(database, callback_data=f"select_db:{database}")
+        button = InlineKeyboardButton(database, callback_data=f'select_db:{database}')
         buttons.append([button])
 
     return InlineKeyboardMarkup(buttons)
@@ -107,11 +111,12 @@ def create_database_buttons(database_list):
 
 def create_metrics_menu():
     metrics_menu = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Active Sessions", callback_data="active_sessions")],
-        [InlineKeyboardButton("Sessions with LWLock", callback_data="sessions_with_lwlock")],
-        [InlineKeyboardButton("Longest Transaction Duration", callback_data="longest_transaction_duration")],
-        [InlineKeyboardButton("Back", callback_data="back")]
+        [InlineKeyboardButton('Active Sessions', callback_data='active_sessions')],
+        [InlineKeyboardButton('Sessions with LWLock', callback_data='sessions_with_lwlock')],
+        [InlineKeyboardButton('Longest Transaction Duration', callback_data='longest_transaction_duration')],
+        [InlineKeyboardButton('Back', callback_data='back')]
     ])
+
     return metrics_menu
 
 
@@ -119,65 +124,70 @@ async def select_option(update: Update, context: CallbackContext):
     query = update.callback_query
     global selected_database, selected_metric
 
-    if query.data.startswith("select_db:"):
-        selected_database = query.data.split(":")[1]
+    if query.data.startswith('select_db:'):
+        selected_database = query.data.split(':')[1]
         metrics_menu = create_metrics_menu()
-        await query.message.edit_text("Select a metric:", reply_markup=metrics_menu)
-    elif query.data == "back":
+        await query.message.edit_text('Select a metric:', reply_markup=metrics_menu)
+    elif query.data == 'back':
         if selected_metric:
             selected_metric = None
             metrics_menu = create_metrics_menu()
-            await query.message.edit_text("Select a metric:", reply_markup=metrics_menu)
+            await query.message.edit_text('Select a metric:', reply_markup=metrics_menu)
         else:
             selected_database = None
             database_list = get_database_list()
             buttons = create_database_buttons(database_list)
-            await query.message.edit_text("Select a database:", reply_markup=buttons)
-    elif query.data == "active_sessions":
+            await query.message.edit_text('Select a database:', reply_markup=buttons)
+    elif query.data == 'active_sessions':
         selected_metric = query.data
         if selected_database:
             active_sessions_count = get_active_sessions(selected_database)
-            message = f"Active sessions in {selected_database}: {active_sessions_count}"
-            back_button = InlineKeyboardButton("Back", callback_data="back")
+            message = f'Active sessions in {selected_database}: {active_sessions_count}'
+            back_button = InlineKeyboardButton('Back', callback_data='back')
             await query.message.edit_text(message, reply_markup=InlineKeyboardMarkup([[back_button]]))
         else:
-            await query.message.edit_text("Please select a database first.")
-    elif query.data == "sessions_with_lwlock":
+            await query.message.edit_text('Please select a database first.')
+    elif query.data == 'sessions_with_lwlock':
         selected_metric = query.data
         if selected_database:
             sessions_with_lwlock_count = get_sessions_with_lwlock(selected_database)
-            message = f"Sessions with LWLock in {selected_database}: {sessions_with_lwlock_count}"
-            back_button = InlineKeyboardButton("Back", callback_data="back")
+            message = f'Sessions with LWLock in {selected_database}: {sessions_with_lwlock_count}'
+            back_button = InlineKeyboardButton('Back', callback_data='back')
             await query.message.edit_text(message, reply_markup=InlineKeyboardMarkup([[back_button]]))
         else:
-            await query.message.edit_text("Please select a database first.")
-    elif query.data == "longest_transaction_duration":
+            await query.message.edit_text('Please select a database first.')
+    elif query.data == 'longest_transaction_duration':
         selected_metric = query.data
         if selected_database:
             longest_transaction_duration = get_longest_transaction_duration(selected_database)
-            message = f"Longest transaction duration in {selected_database}: {longest_transaction_duration}"
-            back_button = InlineKeyboardButton("Back", callback_data="back")
+            message = f'Longest transaction duration in {selected_database}: {longest_transaction_duration}'
+            back_button = InlineKeyboardButton('Back', callback_data='back')
             await query.message.edit_text(message, reply_markup=InlineKeyboardMarkup([[back_button]]))
         else:
-            await query.message.edit_text("Please select a database first.")
+            await query.message.edit_text('Please select a database first.')
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your PostgreSQL database monitoring bot")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Hello! I'm your PostgreSQL database monitoring bot"
+    )
 
 
 async def stats(update: Update, context: CallbackContext):
     database_list = get_database_list()
     if database_list:
-        message = "Select a database:"
+        message = 'Select a database:'
         buttons = create_database_buttons(database_list)
         await update.message.reply_text(message, reply_markup=buttons)
     else:
-        await update.message.reply_text("No databases found.")
+        await update.message.reply_text('No databases found.')
 
 
 if __name__ == '__main__':
+    # load .env variables
     load_dotenv()
+
     application = ApplicationBuilder().token(os.getenv('API_TOKEN')).build()
 
     start_handler = CommandHandler('start', start)
